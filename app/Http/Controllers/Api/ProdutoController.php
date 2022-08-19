81328c5f42503c955ba726fe614e95d9daf2a73f
cloves-max <clovesjuniorrodrigues@gmail.com>
2022-08-19 13:47:04 -0300

    first commit

---
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Entrada;
+use App\Models\Saida;
+use GuzzleHttp\Handler\Proxy;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //dd('ok');
+        $data = Produto::with('saidas')->get();
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+       //dd($request->all());
+
+        $produto = Produto::create([
+            'nome' => $request->name,
+            'descricao' => $request->descricao,
+            'dataEntrada' => $request->dataEntrada
+        ]);
+
+        return response()->json($produto, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $produto = Produto::whereId($id)->first();
+        return response()->json($produto);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        // dd($produto);
+        $validated = $request->validate([
+            'nome' => 'nullable|string',
+            'descricao' => 'nullable|string',
+            'dataEntrada' => 'nullable|date',
+            'dataSaida' => 'nullable|date',
+            'tipo' => 'nullable|string'
+        ]);
+
+           $produto->update($validated);
+
+        if($request->exists('dataSaida')){
+            $produto->saidas()->create($validated);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Produto::destroy($id);
+        return response()->noContent();
+    }
+}
---
+++ database/migrations/2022_08_17_144206_create_saidas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saidas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('produto_id');
+            $table->string('tipo')->nullable();
+            $table->dateTime('dataSaida')->nullable();
+            $table->timestamps();
+
+
+            $table->foreign('produto_id')->references('id')->on('produtos');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saidas');
+    }
+}
